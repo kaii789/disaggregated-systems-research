44ec732697d5fb3e73ece187861eae462c2d5b84
Kailong Huang <kailong9@gmail.com>
2021-06-26 11:26:41 -0400

    Fix gran experriment

--- experiments/compression/granularity.py
+++ experiments/compression/granularity.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 from base import *
 
-def run_gran(config_name, benchmark_name, x_axis, x_axis_label, x_axis_config_param, program_command, result_name, cwd=None
+def run_gran(config_name, compression_name, benchmark_name, x_axis, x_axis_label, x_axis_config_param, program_command, result_name, cwd=None
 ):
     # Multithread
     res1 = []
-    t1 = threading.Thread(target=thread_experiment, args=(x_axis_label, x_axis, "bdi-{}".format(benchmark_name), os.path.abspath(config_name), x_axis_config_param, program_command, res1, cwd))
+    t1 = threading.Thread(target=thread_experiment, args=(x_axis_label, x_axis, "{}-{}".format(compression_name, benchmark_name), os.path.abspath(config_name), x_axis_config_param, program_command, res1, cwd))
 
     t1.start()
     t1.join()
@@ -26,22 +26,24 @@ if __name__ == "__main__":
     x_axis_label = "Compression Granularity(bytes)"
     x_axis_config_param = "perf_model/dram/compression_model/compression_granularity"
     program_info = [
-        # ("sssp", "../../../test/crono/apps/sssp/sssp ../../../test/crono/inputs/bcsstk05.mtx 1", None),
+        ("sssp", "../../../test/crono/apps/sssp/sssp ../../../test/crono/inputs/bcsstk05.mtx 1", None),
         # ("ligra-bfs", "../../../benchmarks/ligra/apps/BFS -s -rounds 1 ../../../benchmarks/ligra/inputs/rMat_1000000", None),
-        ("ligra-pagerank", "../../../benchmarks/ligra/apps/PageRank -s -rounds 1 ../../../benchmarks/ligra/inputs/rMat_1000000", None),
-        ("tinynet", "./darknet classifier predict cfg/imagenet1k.data cfg/darknet19.cfg tiny.weights data/dog.jpg", "../../benchmarks/darknet")
+        # ("ligra-pagerank", "../../../benchmarks/ligra/apps/PageRank -s -rounds 1 ../../../benchmarks/ligra/inputs/rMat_1000000", None),
+        # ("tinynet", "./darknet classifier predict cfg/imagenet1k.data cfg/darknet19.cfg tiny.weights data/dog.jpg", "../../benchmarks/darknet")
     ]
 
     # For each benchmark, launch sweep w/ bdi
-    bdi_gran = [32, 64, 128, 256]
+    bdi_gran = [32, 64, 128]
     result_name = "bdi_ipc_vs_compression_gran"
+    compression_name = "bdi"
     for benchmark_name, program_command, cwd in program_info:
-        t = threading.Thread(target=run_gran, args=("./bdi", benchmark_name, bdi_gran, x_axis_label, x_axis_config_param, program_command, result_name, cwd))
+        t = threading.Thread(target=run_gran, args=("./bdi", compression_name, benchmark_name, bdi_gran, x_axis_label, x_axis_config_param, program_command, result_name, cwd))
         t.start()
 
     # For each benchmark, launch sweep w/ lz4
     lz4_gran = [1024, 2048, 4096]
     result_name = "lz4_ipc_vs_compression_gran"
+    compression_name = "lz4"
     for benchmark_name, program_command, cwd in program_info:
-        t = threading.Thread(target=run_gran, args=("./lz4", benchmark_name, lz4_gran, x_axis_label, x_axis_config_param, program_command, result_name, cwd))
+        t = threading.Thread(target=run_gran, args=("./lz4", compression_name, benchmark_name, lz4_gran, x_axis_label, x_axis_config_param, program_command, result_name, cwd))
         t.start()
