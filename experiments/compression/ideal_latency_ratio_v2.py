66c4c0b8d4e6329ef9c83bded393963b7bdbcbb4
Christina Giannoula <c.giannoula@mel-15.syslab.sandbox>
2021-07-06 15:26:28 -0400

    bug fix

--- experiments/compression/ideal_latency_ratio_v2.py
+++ experiments/compression/ideal_latency_ratio_v2.py
@@ -96,18 +96,22 @@ if __name__ == "__main__":
     x_axis_config_param2 = "perf_model/dram/compression_model/ideal/decompression_latency"
     x_axis_config_param3 = "perf_model/dram/compression_model/ideal/compressed_page_size"
     program_command = "../../../test/crono/apps/sssp/sssp_int ../../../test/crono/inputs/roadNet-PA.mtx 1"
-    result_name = "ipc_vs_compression_latency"
+    result_name = "ipc_vs_compression_latency-sssp"
     t1 = threading.Thread(target=run_ideal_ratio, args=(latency, compressed_page_size, x_axis_label, x_axis_config_param, x_axis_config_param2, x_axis_config_param3, program_command, result_name, "sssp"))
     t1.start()
+    t1.join()
 
     # Ligra
+    result_name = "ipc_vs_compression_latency-bfs"
     program_command = "../../../benchmarks/ligra/apps/BFS -s -rounds 1 ../../../benchmarks/ligra/inputs/rMat_1000000" # TODO: change me
     t2 = threading.Thread(target=run_ideal_ratio, args=(latency, compressed_page_size, x_axis_label, x_axis_config_param, x_axis_config_param2, x_axis_config_param3, program_command, result_name, "bfs"))
     t2.start()
+    t2.join()
 
     # Darknet
-    rogram_command = "./darknet classifier predict cfg/imagenet1k.data cfg/darknet19.cfg tiny.weights data/dog.jpg" # TODO: change me
+    result_name = "ipc_vs_compression_latency-darknet"
+    program_command = "./darknet classifier predict cfg/imagenet1k.data cfg/darknet19.cfg tiny.weights data/dog.jpg" # TODO: change me
     cwd = "../../benchmarks/darknet"
     t3 = threading.Thread(target=run_ideal_ratio, args=(latency, compressed_page_size, x_axis_label, x_axis_config_param, x_axis_config_param2, x_axis_config_param3, program_command, result_name, "darknet", cwd))
-
     t3.start()
+    t3.join()
