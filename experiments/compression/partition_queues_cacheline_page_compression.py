a46aa3dc09fc810ae12c08fee8c3f17830526b28
Kailong Huang <kailong9@gmail.com>
2021-07-08 15:05:51 -0400

    Add stats script

--- experiments/compression/partition_queues_cacheline_page_compression.py
+++ experiments/compression/partition_queues_cacheline_page_compression.py
@@ -10,7 +10,7 @@ import threading
 
 import sys
 sys.path.insert(1, '../../disaggr_scripts')
-import sniper_lib
+import stats
 import run_sniper_repeat_base as automation
 
 # Sweep BW 4, 32
@@ -180,7 +180,7 @@ command_strs["ligra_bfs_small_input_localdram_1MB"] = ligra_base_str_options.for
 )
 
 
-# TODO: BFS, 8 MB
+# BFS, 8 MB
 def run_bfs(ligra_input_selection, num_MB):
     experiments = []
     ligra_input_file = ligra_input_to_file[ligra_input_selection]
@@ -220,7 +220,7 @@ def run_bfs(ligra_input_selection, num_MB):
     return experiments
 
 
-# TODO: Darknet tiny, 2 MB
+# Darknet tiny, 2 MB
 def run_tinynet(model_type):
     experiments = []
     net_lat = 120
@@ -252,7 +252,7 @@ def run_tinynet(model_type):
     return experiments
 
 
-# TODO: Stream, 2 MB
+# Stream, 2 MB
 def run_stream(type):
     experiments = []
     net_lat = 120
@@ -284,7 +284,7 @@ def run_stream(type):
     return experiments
 
 
-# TODO: sssp 256KB
+# sssp 256KB
 def run_sssp(input):
     experiments = []
     net_lat = 120
@@ -304,7 +304,7 @@ def run_sssp(input):
 
                 experiments.append(
                     automation.Experiment(
-                        experiment_name="sssp_{}_localdram_{}_netlat_{}_bw_scalefactor_{}_remoteinit_{}_pq_cacheline_combined_series".format(
+                        experiment_name="sssp_{}_localdram_{}_netlat_{}_bw_scalefactor_{}_combo".format(
                             input,
                             localdram_size_str,
                             net_lat,
@@ -319,6 +319,86 @@ def run_sssp(input):
 
     return experiments
 
+def graph(res_name, benchmark_list, local_dram_list, bw_scalefactor_list):
+    labels = ["Remote Bandwidth Scalefactor", "Compression Off", "Page Compression(ideal)", "Cacheline + Page Compression(ideal)", "Page Compression", "Cacheline + Page Compression"]
+    # "stream_{}_localdram_{}_netlat_{}_bw_scalefactor_{}_combo"
+
+    process = 0
+    num_bars = len(labels) - 1
+    res = [bw_scalefactor_list[:]] + [[] for _ in range(num_bars)]
+    compression_res = {}
+    for benchmark in benchmark_list:
+        for size in local_dram_list:
+            for factor in bw_scalefactor_list:
+                dir1 = "{}localdram_{}_netlat_120_bw_scalefactor_{}_combo_output_files".format(benchmark, size, factor)
+                for run in range(1, 1 + num_bars):
+                    dir2 = "run_{}_process_{}_temp".format(run, process)
+                    res_dir = "./{}/{}".format(dir1, dir2)
+                    ipc = stats.get_ipc(res_dir)
+                    res[run].append(ipc)
+
+                    # Compression res
+                    if run in range(2, 1 + num_bars):
+                        type = "{}-{}".format(run - 1, factor)
+                        compression_res[type] = {}
+                        cr, cl, dl, ccr, ccl, cdl = stats.get_compression_stats(res_dir)
+                        compression_res[type]['Compression Ratio'] = cr
+                        compression_res[type]['Compression Latency'] = cl
+                        compression_res[type]['Decompression Latency'] = dl
+                        if ccr:
+                            compression_res[type]['Cacheline Compression Ratio'] = ccr
+                            compression_res[type]['Cacheline Compression Latency'] = ccl
+                            compression_res[type]['Cacheline Decompression Latency'] = cdl
+
+                    process += 1
+
+    data = {labels[i]: res[i] for i in range(len(labels))}
+    # print(data)
+    print(compression_res)
+    df = pd.DataFrame(data)
+    graph = df.plot(x=labels[0], y=labels[1:], kind="bar", title=res_name)
+    fig = graph.get_figure()
+    fig.savefig(res_name)
+
+def gen_settings_for_graph(benchmark_name):
+    if benchmark_name == "sssp":
+        res_name = "sssp_262144B_combo"
+        benchmark_list = []
+        for input in ["bcsstk05.mtx"]:
+            benchmark_list.append("sssp_{}_".format(input))
+        local_dram_list = ["262144B"]
+        bw_scalefactor_list = [4, 32]
+    elif benchmark_name == "bfs_reg":
+        res_name = "bfs_reg_8MB_combo"
+        benchmark_list = []
+        benchmark_list.append("ligra_{}_".format("regular_input"))
+        local_dram_list = [8]
+        bw_scalefactor_list = [4, 32]
+    elif benchmark_name == "tinynet":
+        res_name = "tinynet_2MB_combo"
+        benchmark_list = []
+        for model in ["tinynet"]:
+            benchmark_list.append("darknet_{}".format(model))
+        local_dram_list = [2]
+        bw_scalefactor_list = [4, 32]
+    elif benchmark_name == "darknet19":
+        res_name = "tinynet_2MB_combo"
+        benchmark_list = []
+        for model in ["darknet19"]:
+            benchmark_list.append("darknet_{}".format(model))
+        local_dram_list = [2]
+        bw_scalefactor_list = [4, 32]
+    elif benchmark_name == "stream_1":
+        res_name = "stream_1_2MB_combo"
+        benchmark_list = []
+        benchmark_list.append("stream_1")
+        local_dram_list = [2]
+        bw_scalefactor_list = [4, 32]
+
+    return res_name, benchmark_list, local_dram_list, bw_scalefactor_list
+
+
+# TODO: Experiment run
 experiments = []
 # experiments.extend(run_bfs("regular_input", 8))
 # experiments.extend(run_tinynet("tiny"))
@@ -329,23 +409,27 @@ experiments = []
 # experiments.extend(run_bfs("reg_8x", 32))
 # experiments.extend(run_tinynet("resnet50"))
 
-log_filename = "run-sniper-repeat2_1.log"
-num = 2
-while os.path.isfile(log_filename):
-    log_filename = "run-sniper-repeat2_{}.log".format(num)
-    num += 1
+# log_filename = "run-sniper-repeat2_1.log"
+# num = 2
+# while os.path.isfile(log_filename):
+#     log_filename = "run-sniper-repeat2_{}.log".format(num)
+#     num += 1
 
-with open(log_filename, "w") as log_file:
-    log_str = "Script start time: {}".format(automation.datetime.datetime.now().astimezone())
-    print(log_str)
-    print(log_str, file=log_file)
+# with open(log_filename, "w") as log_file:
+#     log_str = "Script start time: {}".format(automation.datetime.datetime.now().astimezone())
+#     print(log_str)
+#     print(log_str, file=log_file)
 
-    experiment_manager = automation.ExperimentManager(
-        output_root_directory=".", max_concurrent_processes=16, log_file=log_file
-    )
-    experiment_manager.add_experiments(experiments)
-    experiment_manager.start()
+#     experiment_manager = automation.ExperimentManager(
+#         output_root_directory=".", max_concurrent_processes=16, log_file=log_file
+#     )
+#     experiment_manager.add_experiments(experiments)
+#     experiment_manager.start()
 
-    log_str = "Script end time: {}".format(automation.datetime.datetime.now().astimezone())
-    print(log_str)
-    print(log_str, file=log_file)
+#     log_str = "Script end time: {}".format(automation.datetime.datetime.now().astimezone())
+#     print(log_str)
+#     print(log_str, file=log_file)
+
+# TODO: Generate graph
+res_name, benchmark_list, local_dram_list, bw_scalefactor_list = gen_settings_for_graph("sssp")
+graph(res_name, benchmark_list, local_dram_list, bw_scalefactor_list)
