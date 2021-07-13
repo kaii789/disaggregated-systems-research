d8622d8414701a84fd7aa8473cd104c48c8bc16c
Christina Giannoula <cgiannoula>
2021-07-13 10:06:35 +0000

    added nw

--- disaggr_scripts/run_experiments_cgiannoula.py
+++ disaggr_scripts/run_experiments_cgiannoula.py
@@ -29,6 +29,10 @@ if __name__ == "__main__":
         sniper_root=subfolder_sniper_root_relpath
     )
 
+    nw_base_options = "{sniper_root}/run-sniper -d {{{{sniper_output_dir}}}} -c {sniper_root}/disaggr_config/local_memory_cache.cfg -c repeat_testing.cfg {{sniper_options}} -- {sniper_root}/benchmarks/rodinia/bin/needle {{0}} 1 1".format(
+        sniper_root=subfolder_sniper_root_relpath
+    )
+
     ###  Darknet command strings  ###
     # Note: using os.system(), the 'cd' of working directory doesn't persist to the next call to os.system()
     darknet_base_options = "cd {1} && ../../run-sniper -d {{{{sniper_output_dir}}}} -c ../../disaggr_config/local_memory_cache.cfg -c {{{{sniper_output_dir}}}}/repeat_testing.cfg {{sniper_options}} -- {0}/darknet classifier predict {0}/cfg/imagenet1k.data {0}/cfg/{{0}}.cfg {0}/{{0}}.weights {0}/data/dog.jpg".format(
@@ -324,6 +328,33 @@ if __name__ == "__main__":
                     output_root_directory=".",
                 )
             )
+
+    # rodinia-nw needle 6144, bw_factor [4, 16] 
+    nw_compression_experiments_remoteinit_true = []
+    for remote_init in ["true"]:  # "false"
+        for bw_scalefactor in [4, 16]:
+            command_str = nw_base_options.format(
+                "6144",
+                sniper_options="-g perf_model/dram/remote_mem_bw_scalefactor={} -g perf_model/dram/remote_init={} -s stop-by-icount:{}".format(
+                    int(bw_scalefactor),
+                    str(remote_init),
+                    int(1 * ONE_BILLION),
+                ),
+            )
+
+            nw_compression_experiments_remoteinit_true.append(
+                Experiment(
+                    experiment_name="nw_6144_bw_scalefactor_{}_remoteinit_{}_compression_series".format(
+                        bw_scalefactor,
+                        remote_init,
+                    ),
+                    command_str=command_str,
+                    experiment_run_configs=compression_series_experiment_run_configs,
+                    output_root_directory=".",
+                )
+            )
+
+
 
     # darknet bw_factor [4, 16] 
     darknet_compression_experiments_remoteinit_true = []
